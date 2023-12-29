f717f8659a5c549456742666a3777a3bf4e8013a
Angel <araozangel842@gmail.com>
2023-12-29 15:42:28 +0200

    Testing connection

--- client/App.js
+++ client/App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { UseConnection } from './hooks/useConnection';
 
 export default function App() {
+  const { connection } = UseConnection();
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!!!</Text>
+      {connection ? <Text>Connected</Text> : <Text>Not Connected</Text>}
       <StatusBar style="auto" />
     </View>
   );
