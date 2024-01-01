1533a91f063affb2b067414049fdcfe699486e47
Angel <araozangel842@gmail.com>
2024-01-01 15:36:09 +0200

    Delete unnecessary imports

--- client/context/AuthContext.js
+++ client/context/AuthContext.js
@@ -1,5 +1,3 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { createContext } from 'react';
 
-
-export const AuthContext = React.createContext()
+export const AuthContext = createContext();
