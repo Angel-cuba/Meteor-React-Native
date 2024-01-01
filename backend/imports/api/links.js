3015dcc1a234190af9b36bf44a47929cc44559a8
Angel <araozangel842@gmail.com>
2024-01-01 13:29:43 +0200

    login flow not working

--- backend/imports/api/links.js
+++ backend/imports/api/links.js
@@ -1,3 +1,6 @@
 import { Mongo } from 'meteor/mongo';
 
 export const LinksCollection = new Mongo.Collection('links');
+
+export const Users = new Mongo.Collection('user');
+
--- backend/server/main.js
+++ backend/server/main.js
@@ -1,5 +1,8 @@
 import { Meteor } from 'meteor/meteor';
 import { LinksCollection } from '/imports/api/links';
+import { check, Match } from 'meteor/check';
+import { Accounts } from 'meteor/accounts-base';
+import { Users } from '../imports/api/links';
 
 async function insertLink({ title, url }) {
   await LinksCollection.insertAsync({ title, url, createdAt: new Date() });
@@ -7,7 +10,7 @@ async function insertLink({ title, url }) {
 
 Meteor.startup(async () => {
   // If the Links collection is empty, add some data.
-  if (await LinksCollection.find().countAsync() === 0) {
+  if ((await LinksCollection.find().countAsync()) === 0) {
     await insertLink({
       title: 'Do the Tutorial',
       url: 'https://react-tutorial.meteor.com/simple-todos/01-creating-app.html',
@@ -31,7 +34,49 @@ Meteor.startup(async () => {
 
   // We publish the entire Links collection to all clients.
   // In order to be fetched in real-time to the clients
-  Meteor.publish("links", function () {
+  Meteor.publish('links', function () {
     return LinksCollection.find();
   });
+});
+
+//function to register a user
+Meteor.methods({
+  register: async function ({ email, password }) {
+    console.log('🚀 ~ file: main.js:45 ~ password:', password);
+    console.log('🚀 ~ file: main.js:45 ~ email:', email);
+    // const { email, password } = email1;
+    try {
+      // check({
+      //   email: String,
+      //   password: String,
+      // });
+    } catch (err) {
+      throw new Meteor.Error(err.message);
+    }
+    // const userFound = Accounts.findUserByEmail(email);
+
+    // if (userFound) {
+    //   throw new Meteor.Error('Email already exists');
+    // }
+    const newUser = {
+      email,
+      password,
+    };
+    const userInfo = Users.insert(newUser);
+    console.log("🚀 ~ file: main.js:66 ~ userInfo:", userInfo)
+    return userInfo;
+    // const userId = Accounts.createUser({
+    //   email,
+    //   password,
+    // });
+
+    // Usar Accounts.createUser para registrar usuarios
+    // const userId = Accounts.createUser({
+    //   email,
+    //   password,
+    // });
+
+    // // Puedes devolver el userId o cualquier otra información que desees
+    // return userId;
+  },
 });
